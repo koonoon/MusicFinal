9d922464ce4a60eda0482575da111e5caffbc725
koonoon <cktogks@gmail.com>
2022-06-10 11:25:51 +0900

    commit

---
+++ MusicWired/src/main/java/com/icia/musicwired/MusicWiredApplication.java
@@ -0,0 +1,13 @@
+package com.icia.musicwired;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicWiredApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicWiredApplication.class, args);
+	}
+
+}
